4de99051e1503fc133058b2ccc9519342d59414c
agalarza768 <adriangalarza1578@gmail.com>
2024-10-28 18:12:06 -0300

    Agrega estado a las campañas, un monto actual y el address de su creador. Tambien agrega require en setCampaign para verificar que el target_amount sea válido, y agrega funciones "contribute" y "cancelCampaign".

--- contracts/campaigns.sol
+++ contracts/campaigns.sol
@@ -3,22 +3,30 @@ pragma solidity ^0.8.0;
 
 contract Campaigns {
 
+    enum CampaignState { Active, Succeful, Failed }
+
     struct Campaign {
         uint target_amount;
+        uint current_amount;
         uint deadline;
         uint creationDate;
+        address creator;
+        CampaignState state;
     }
 
     uint public totalCampaigns;
     mapping(uint => Campaign) public campaigns;
     
     function setCampaign(uint _target_amount, uint _days_deadline) public {
-
+        require(_target_amount > 0, "Target amount must be greater than zero ");
         totalCampaigns++;
         campaigns[totalCampaigns] = Campaign({
             target_amount: _target_amount,
+            current_amount: 0,
             deadline: _days_deadline,
-            creationDate: block.timestamp
+            creationDate: block.timestamp,
+            creator: msg.sender,
+            state: CampaignState.Active
         });
     }
 
@@ -30,5 +38,25 @@ contract Campaigns {
         }
         
         return allCampaigns;
+    }
+
+    function contribute(uint _campaignId, uint _amount) external view { 
+        Campaign memory campaign = campaigns[_campaignId];
+        require(campaign.state == CampaignState.Active, "Can only contribute to active campaigns");
+
+        // ...
+
+        campaign.current_amount += _amount;
+    }
+
+    function cancelCampaign(uint _campaignId) external {
+        Campaign memory campaign = campaigns[_campaignId];
+        require(campaign.creator == msg.sender, "Only the creator of the campaign can cancel it");
+        require(campaign.state == CampaignState.Active, "Only active campaigns can be cancelled");
+
+        // ...
+
+        delete campaigns[_campaignId];
+        totalCampaigns--;
     }
 }
