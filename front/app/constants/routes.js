18245a2c730f18b5f72a4c5f9b68091ed8aeb550
Francisco <fnasif@fi.uba.ar>
2024-11-08 17:54:39 -0300

    Division en Front y Back + Creacion front

---
+++ front/app/constants/routes.js
@@ -0,0 +1,16 @@
+const ROUTES = {
+  MY_CAMPAIGNS: {
+    href: "/my-campaigns",
+    label: "Mis Campañas",
+  },
+  CONTRIBUTIONS: {
+    href: "/contributions",
+    label: "Contribuciones",
+  },
+  RANKING: {
+    href: "/ranking",
+    label: "Ranking",
+  },
+};
+
+export default ROUTES;
---
+++ front/app/my-campaigns/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyCampaignsPage = () => {
+  return (
+    <div>
+      <h1>My Campaigns</h1>
+      <p>LISTADO DE CAMPAÑAS</p>
+    </div>
+  );
+};
+
+export default MyCampaignsPage;
