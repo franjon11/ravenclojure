a012c3a49319a004b66fc06e5ab2f0da1d179d99
danielagenaro98 <danielagenaro98@gmail.com>
2024-11-09 17:20:14 -0300

    conexión del front con el contrato de campañas

--- front/app/my-campaigns/page.js
+++ front/app/my-campaigns/page.js
@@ -1,6 +1,28 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import campaignsContractABI from "../../campaignsContract.json";
+
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+const contractAddress = "0xcD6a42782d230D7c13A74ddec5dD140e55499Df9";
+const campaignsContract = new web3.eth.Contract(
+  campaignsContractABI,
+  contractAddress
+);
 
 const MyCampaignsPage = () => {
+  const [campaigns, setCampaigns] = useState([]);
+
+  useEffect(() => {
+    async function fetchCampaigns() {
+      const contract = await campaignsContract.methods.getCampaigns();
+      setCampaigns(contract);
+    }
+    fetchCampaigns();
+  }, []);
+
+  console.log(campaigns);
+
   return (
     <div>
       <h1>My Campaigns</h1>
